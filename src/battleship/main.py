4142ddecde4c302ced0c32a6d49f41df87487e53
Jacob Leehy <117180669+Jleehy@users.noreply.github.com>
2024-09-29 21:39:40 -0500

    Update main.py

--- src/battleship/main.py
+++ src/battleship/main.py
@@ -3,35 +3,35 @@ This file serves as the main game entrypoint.
 It takes the number of ships to play the game with from the
 user, validates input, and the starts the gameplay loop.
 """
-import sys
-from game import Game
+import sys #import for sys
+from game import Game # import game
 
-def main():
+def main(): #def main func
 
     #print(sys.argv[1])
-    while True:
-        try:
+    while True: # while true
+        try: #try
             #If something that cant be interpreated as an int is input, a ValueError will be raised.
-            num_ships: int = int(input('How many ships? (1-5): '))
+            num_ships: int = int(input('How many ships? (1-5): ')) # get num ships
 
-        except ValueError:
-            print('Please input a number.')
+        except ValueError: #raise error if needed
+            print('Please input a number.') # ask reinput
 
             #reprompt the user.
-            continue
+            continue # continue
         
         #reprompt the user if an invalid int is input.
-        if not 1 <= num_ships  <= 5:
-            print('Please input a number between 1 and 5.')
-            continue
+        if not 1 <= num_ships  <= 5: #check valid
+            print('Please input a number between 1 and 5.') #tell bad
+            continue #continue
 
-        break
+        break #break
 
-    Game(num_ships).loop()
+    Game(num_ships).loop() #call loop
 
-if __name__ == '__main__':
-    try:
-        main()
+if __name__ == '__main__': #main magic
+    try: #try
+        main() #call main
     
     #catch SIGINT and print goodbye message. Extra newline for formatting on some terminals.
     except KeyboardInterrupt:
